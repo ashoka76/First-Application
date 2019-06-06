9128adba34128bc2d610e8cd0a1ea8a1c33e16f2
ashoka <ashoka@ibiMarketing10.infobrain.com>
2019-06-06 16:20:23 +0530

    commited location related changes

--- src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -2,6 +2,7 @@ package com.tutorialspoint.struts2;
 
 public class HelloWorldAction {
    private String name;
+   private String location;
 
    public String execute() throws Exception {
       System.out.println("Execute method");
@@ -15,4 +16,7 @@ public class HelloWorldAction {
    public void setName(String name) {
       this.name = name;
    }
+   public void setLocation(String location) {
+	      this.location = location;
+	   }
 }
